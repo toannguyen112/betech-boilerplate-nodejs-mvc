9f2e4f30ba6bc4667d8a6983f3976c4fdffdbf81
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-08-11 00:46:13 +0700

    added security.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const host = process.env.DB_HOST;
 const username = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
+const password = process.env.DB_PASS;
 const database = process.env.DB_NAME;
 
 module.exports = {
