0e3fddfbc820ecaab13633b4668f8c6777243cd6
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-08-11 00:43:42 +0700

    Fixed so the code compiles

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const host = process.env.DB_HOST;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+
+module.exports = {
+  dialect: "postgres",
+  host,
+  username,
+  password,
+  database,
+  define: {
+    timestamps: true,
+  },
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
