dceed4c2d8ca1ea4552db230b569fa90506fd04f
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-08-10 17:08:54 +0700

    removed echo and die statements, lolz.

--- src/services/express.service.ts
+++ src/services/express.service.ts
@@ -7,16 +7,11 @@ import bodyParser from "body-parser";
 let server = express();
 let routes: any[] = [];
 let routeFiles: string[] = []
-if (server.get('env') === 'development') {
-  routeFiles = fs.readdirSync(__dirname + "/../routes/").filter((file) => file.endsWith(".ts"));
-}
-else {
-  routeFiles = fs.readdirSync(__dirname + "/../routes/").filter((file) => file.endsWith(".js"));
-}
+
+routeFiles = fs.readdirSync(__dirname + "/../routes/").filter((file) => file.endsWith(".js") || file.endsWith(".ts"));
 export default class ExpressService {
   static async init() {
     try {
-
 
       for (const file of routeFiles) {
         const route = await import(`../routes/${file}`);
@@ -28,9 +23,6 @@ export default class ExpressService {
       server.use(bodyParser.urlencoded({ extended: true }));
       server.use(bodyParser.json());
       server.use(express.json());
-      server.get("/", (req, res) => {
-        res.json("test");
-      })
       server.use(routes);
       server.listen(env.PORT || 8000);
 
