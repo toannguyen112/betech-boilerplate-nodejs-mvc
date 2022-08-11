c5df8d146072d1d8d30df2553d186248725bab1d
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-08-11 13:40:32 +0700

    yolo push

--- src/controllers/Product/product.controller.ts
+++ src/controllers/Product/product.controller.ts
@@ -30,6 +30,30 @@ export default class ProductController {
     }
   }
 
-  async update(req: Request, res: Response) { }
-  async delete(req: Request, res: Response) { }
+  async update(req: Request, res: Response) {
+    try {
+      const { t_schema_id } = req.tenant_user;
+      const { id, body } = req.params;
+      const data = await Product.update({ body }, {
+        where: { t_prod_id: id, t_prod_schemaID: t_schema_id }
+      });
+
+      return res.status(200).json({ message: "OK", data });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+  async delete(req: Request, res: Response) {
+    try {
+      const { t_schema_id } = req.tenant_user;
+      const { id } = req.params;
+      const data = await Product.destroy({
+        where: { t_prod_id: id, t_prod_schemaID: t_schema_id }
+      });
+
+      return res.status(200).json({ message: "OK", data });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
 }
