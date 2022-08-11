7f3e8b7a44077a64799925a2f129fe2b559729e1
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-08-11 13:25:56 +0700

    hmmm

--- src/controllers/Product/product.controller.ts
+++ src/controllers/Product/product.controller.ts
@@ -2,14 +2,12 @@ import { Request, Response } from "express";
 import Product from "../../models/product.model";
 import ProductCategory from "../../models/product_category.model";
 export default class ProductController {
-
   async index(req: Request, res: Response) {
     try {
       const { t_schema_id } = req.tenant_user;
-
       const data = await Product.findAll({
         where: { t_prod_schemaID: t_schema_id },
-        include: ProductCategory
+        include: ProductCategory,
       });
 
       return res.status(200).json({ message: "OK", data });
@@ -21,20 +19,12 @@ export default class ProductController {
   async create(req: Request, res: Response) {
     try {
       const { t_schema_id } = req.tenant_user;
-
       const data = await Product.create({
         t_prod_schemaID: t_schema_id,
-        t_prod_name: `Product 1`,
-        t_prod_size: "big",
-        t_prod_code: "USR202rLJUUI393mMK",
-        t_prod_sku: "USR202rLJUUI393mMK",
-        t_prod_gender: "USR202rLJUUI393mMK",
-        t_prod_color: "USR202rLJUUI393mMK",
-        t_prod_ageFrom: 1,
-        t_prod_ageTo: 2
+        ...req.body,
       });
 
-      return res.status(200).json(data);
+      return res.status(200).json({ message: "OK", data });
     } catch (error) {
       res.status(500).send(error);
     }
