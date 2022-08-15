3ffc0edf4f469ae29ea85b0191f84e87fcf025e7
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-08-15 17:26:38 +0700

    code

--- src/controllers/helper.controller.ts
+++ src/controllers/helper.controller.ts
@@ -1,1 +1,44 @@
-class HelperController { }
+import express from "express";
+export default class HelperController {
+    static getModelData(req, res) {
+        return "123";
+
+        let model;
+        let items;
+        let method;
+
+        if (req.body.const) {
+            model = this.modelNamespace(req);
+            items = `${items}/${model}`;
+        }
+        else {
+            model = this.modelNamespace(req);
+            model = new model;
+            method = req.body.method;
+        }
+
+        if (req.body.params) {
+            items = model.method(req.body.params);
+        }
+        else {
+            items = model.method;
+        }
+
+        items = this.onlyFields(req, items);
+    }
+
+    static modelNamespace(req: express.Request) {
+        const model = req.body.model;
+        return `src/models/${model}`;
+    }
+
+    static onlyFields(req: express.Request, items) {
+        if (req.body.only) {
+            return items.only(req.body.only);
+        } else if (req.body.method === 'get') {
+            return items.only('id', req.body.label, req.body.name);
+        }
+
+        return items;
+    }
+}
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,37 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import HomeController from "../controllers/home.controller";
 
 const route: Router = Router();
 
 route.get("/", new HomeController().index);
+route.post("/model-data", (req: Request, res: Response) => {
+    getModelData(req, res);
+});
+
+async function getModelData(req: Request, res: Response) {
+    let model: any;
+    model = await modelNamespace(req);
+    let items = await onlyFields(req, model);
+
+    return res.json(items);
+}
+
+async function modelNamespace(req: Request): Promise<any> {
+    const model = req.body.model;
+    const modelName = await import(`../models/${model}.model.ts`);
+    return modelName.default;
+}
+
+async function onlyFields(req: Request, model) {
+    let items;
+    if (req.body.only) {
+        const arrOnly: string[] = Object.values(req.body.only);
+        items = await model.findAll({ attributes: arrOnly });
+    } else if (req.body.method === "get") {
+        items = await model.findAll({});
+    }
+
+    return items;
+}
 
 export default route;
