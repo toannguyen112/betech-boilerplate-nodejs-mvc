dbac45f98382f1e6b253858414fa4100da2aa8f8
Nguyen Chi Cuong <nguyenchicuong31998@gmail.com>
2022-08-17 08:47:15 +0700

    add-folder-test-for-project

---
+++ src/test/tenant/tenant.test.ts
@@ -0,0 +1,17 @@
+const request = require("supertest");
+require("dotenv").config();
+const BASE_URL = process.env.BASE_URL;
+
+describe("/tenants", () => {
+  it("/get-tenants", async () => {
+    expect(1 + 2).toBe(3);
+
+    const tenant = await request(BASE_URL)
+                  .get("/tenants");
+    // const { token } =  await request(BASE_URL).get("/token");
+    // const tenant = await request(BASE_URL)
+    //               .get("/tenants")
+    //               .set('Authorization', `Bearer ${token}`);;
+    console.log("Tenant", JSON.stringify(tenant.body));
+  });
+});
